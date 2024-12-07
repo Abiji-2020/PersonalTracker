59a21707f3ab8f6be30abe80a62e24f87cea5978
Abinand P <abinand0911@gmail.com>
2024-12-07 12:23:15 +0530

    feat: added temp checkbox component
    Signed-off-by: Abinand P <abinand0911@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-
-
+import CheckBoxCard from './components/checkbox'
+import { MantineProvider } from '@mantine/core'
+import './App.css'
+import '@mantine/core/styles.css'
 function App() {
 
   return (
-    <>
-     
-    </>
+    <MantineProvider>
+      <div>
+        <CheckBoxCard />
+      </div>
+    </MantineProvider>
   )
 }
 
