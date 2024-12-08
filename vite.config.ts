dba1739168ba0ab5b6aa8fa33e1bd498dbc1dcef
Abinand P <abinand0911@gmail.com>
2024-12-08 21:50:06 +0530

    fix
    Signed-off-by: Abinand P <abinand0911@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // Ensure the output directory is correct
+  },
+  server: {
+    open: true, // Optional: Automatically opens the app in the browser
+  },
 });
